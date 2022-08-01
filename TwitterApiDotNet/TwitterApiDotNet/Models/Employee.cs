fafb8f86ca9179f76eba01e1674a1d8c9df8eafd
Vivek banka <banka.vivekreddy@gmail.com>
2022-07-31 22:02:41 -0500

    Added the Entityframework inmemory.

---
+++ TwitterApiDotNet/TwitterApiDotNet/Models/Employee.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwitterApiDotNet.Models
+{
+    public class Employee
+    {
+        public int EmployeeID  {get;set;}
+        public string EmployeeName {get;set;}
+        
+    }
+}
--- TwitterApiDotNet/TwitterApiDotNet/Program.cs
+++ TwitterApiDotNet/TwitterApiDotNet/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TwitterApiDotNet.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<TwitterDbContext>(options =>
+{
+    options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("MyDb"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
