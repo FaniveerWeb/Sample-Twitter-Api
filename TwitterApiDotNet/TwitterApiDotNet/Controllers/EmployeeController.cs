88327dfb40393739645ee6919eb0acd12e275242
vivek.banka <35413989+vivekbanka@users.noreply.github.com>
2022-08-01 16:52:30 -0500

    Added the TwitterStream Controller.

--- TwitterApiDotNet/TwitterApiDotNet/Controllers/EmployeeController.cs
+++ TwitterApiDotNet/TwitterApiDotNet/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TwitterApiDotNet.Models;
 
 namespace TwitterApiDotNet.Controllers
 {
@@ -10,9 +11,29 @@ namespace TwitterApiDotNet.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-        public EmployeeController()
-        {
+        public TwitterDbContext TwitterDbContext { get; }
+        public ILogger Logger { get; }
 
+        public EmployeeController(TwitterDbContext twitterDbContext, ILogger<EmployeeController> logger)
+        {
+            TwitterDbContext = twitterDbContext;
+            Logger = logger;
         }
+
+        [HttpGet("{id}")]
+        public Employee GetEmployeeByID(int id)
+        {
+            Logger.LogInformation("Get the employee information");
+            return TwitterDbContext.Employees.SingleOrDefault(e => e.EmployeeID == id);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Employee employee)
+        {
+            TwitterDbContext.Employees.Add(employee);
+            TwitterDbContext.SaveChanges();
+            return Ok(employee);
+        }
+
     }
 }
---
+++ TwitterApiDotNet/TwitterApiDotNet/Controllers/TwitterStreamController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tweetinvi;
+using Tweetinvi.Models;
+using TwitterApiDotNet.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TwitterApiDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwitterStreamController : ControllerBase
+    {
+        public ILogger<TwitterStreamController> Logger { get; }
+        public TwitterDbContext TwitterDbContext { get; }
+        public IConfiguration Config { get; }
+
+        public TwitterStreamController(ILogger<TwitterStreamController> logger, TwitterDbContext twitterDbContext, IConfiguration config)
+        {
+            Logger = logger;
+            TwitterDbContext = twitterDbContext;
+            Config = config;
+        }
+
+
+        // GET: api/<TwitterStreamController>
+        [HttpGet]
+        public async Task <IActionResult> Get()
+        {
+            try
+            {
+                Logger.LogInformation("Calling tweeter api for streaming");
+                // authenticate the user here 
+                var consumerOnlyCredentials = new ConsumerOnlyCredentials(Config["ClientApiKey"], Config["ClientApiKeySecret"]);
+                var appClientWithoutBearer = new TwitterClient(consumerOnlyCredentials);
+
+                var bearerToken = await appClientWithoutBearer.Auth.CreateBearerTokenAsync();
+                var appCredentials = new ConsumerOnlyCredentials(Config["ClientApiKey"], Config["ClientApiKeySecret"])
+                {
+                    BearerToken = bearerToken
+                };
+
+                var appClient = new TwitterClient(appCredentials);
+                var sampleStreamV2 = appClient.StreamsV2.CreateSampleStream();
+                sampleStreamV2.TweetReceived += (sender, args) =>
+                {
+                    Console.WriteLine(args.Tweet.Entities);
+                };
+
+                await sampleStreamV2.StartAsync();
+                return Ok("test");
+
+            }
+            catch (Exception ex)
+            {
+
+                Logger.LogError(ex.ToString());
+                return StatusCode(500)
+;            }
+        }
+
+       
+    }
+}
